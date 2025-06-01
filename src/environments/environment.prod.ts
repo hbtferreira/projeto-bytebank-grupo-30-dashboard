8dff5e7e0ffb4e67f5ec9949a1007559148f6026
andersonlimadev <andersonlimahw@gmail.com>
2025-05-31 22:52:04 -0300

    🐞 fix: enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   useLocalDb: false,
-  apiBaseUrl: 'http://localhost:3003/api',
+  apiBaseUrl: 'http://localhost:3003',
   apiVersion: '/v1',
   apiNamespace: '/bytebank',
 };
