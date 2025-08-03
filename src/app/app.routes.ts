613df7b274b4c6d655e9deecb1b754d8127a6c2c
Herbert Ferreira <hbtferreira@hotmail.com>
2025-08-02 22:40:12 -0300

    Remove comentários

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent)
-  // },
-  // { path: '**', redirectTo: '' }
-];
+export const routes: Routes = [];
