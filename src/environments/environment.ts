f3f88d5b4982c09c5725011fedb2730fca64025e
Herbert Rezende Ferreira <hbtferreira@hotmail.com>
2025-08-04 02:28:34 -0300

    Substitui as variáveis de dev por prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
+/*
 export const environment = {
   production: false,
   useLocalDb: true,
@@ -6,6 +7,18 @@ export const environment = {
   apiNamespace: '/bytebank',
   shell: {
     remoteEntry: 'http://localhost:4200/remoteEntry.js',
+    remoteName: 'shell-projeto-bytebank-gp-30'
+  }
+};
+*/
+export const environment = {
+  production: true,
+  useLocalDb: false,
+  apiBaseUrl: 'https://bybank-back-end.onrender.com/',
+  apiVersion: '/v1',
+  apiNamespace: '/bytebank',
+  shell: {
+    remoteEntry: 'https://shell-projeto-bytebank-gp-30.vercel.app/remoteEntry.js',
     remoteName: 'shell-projeto-bytebank-gp-30'
   }
 };
